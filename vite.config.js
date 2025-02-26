22dd6bf03d7ae37ec170fc6edc74752d80640781
s00ya <jjschoho@gmail.com>
2025-02-26 15:00:18 +0900

    프론트 기능 추가

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [vue(), vueDevTools()],
+  resolve: {
+    alias: {
+      '@': '/src',  // 그냥 src 폴더를 '@'로 지정
+    },
+  },
+  server: {
+    proxy: {
+      // 프록시할 경로
+      '/api': {
+        // 대상 서버
+        target: 'http://localhost:8080',  // 이곳을 실제 백엔드 서버 IP로 수정
+        // 대상 서버의 호스트 헤더 변경 여부
+        changeOrigin: true,
+        // '/api' 부분 제거
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
